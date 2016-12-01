cce756cedaf32d67bd617da46dfcf28c8d730cdc
Juan Carlos <juancagb.17@gmail.com>
2016-11-30 21:29:06 -0500

    Add trait to user model

--- app/User.php
+++ app/User.php
@@ -3,23 +3,16 @@
 namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use SammyK\LaravelFacebookSdk\SyncableGraphNodeTrait;
 
 class User extends Authenticatable
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    use SyncableGraphNodeTrait;
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
