3913f950d81581af42295127d23a84cbe9a2bcf9
Juan Carlos <juancagb.17@gmail.com>
2016-12-08 20:30:13 -0500

    Test response

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -123,7 +123,6 @@ class PromotionController extends Controller
         Session::put('fb_user_access_token', (string) $token);
 
         // Now we can perform a Facebook SDK request
-        dd($promotion->fanPage);
         $fanPageId = $promotion->fanPage->fan_page_id;
         $query = "/me/likes/$fanPageId";
         try {
