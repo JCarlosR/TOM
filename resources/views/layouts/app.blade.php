d5c9f958142c44015a15f9ba007c0e32f4b7e1ae
Juan Carlos <juancagb.17@gmail.com>
2016-12-15 11:26:15 -0500

    Add spectral design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,74 +9,71 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
     <!-- Styles -->
     <link href="{{ asset('css/bootstrap.css', env('REDIRECT_HTTPS')) }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/main.css', env('REDIRECT_HTTPS')) }}" rel="stylesheet">
 
     @yield('styles')
-
-    <style>
-        body {
-            font-family: 'Lato', serif;
-        }
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
 </head>
-<body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+<body class="landing">
+    {{--<nav class="navbar navbar-default navbar-static-top">--}}
+        {{--<div class="container">--}}
+            {{--<div class="navbar-header">--}}
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+                {{--<!-- Collapsed Hamburger -->--}}
+                {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">--}}
+                    {{--<span class="sr-only">Toggle Navigation</span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                {{--</button>--}}
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    TOM
-                </a>
-            </div>
+                {{--<!-- Branding Image -->--}}
+                {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
+                    {{--TOM--}}
+                {{--</a>--}}
+            {{--</div>--}}
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Inicio</a></li>
-                </ul>
+            {{--<div class="collapse navbar-collapse" id="app-navbar-collapse">--}}
+                {{--<!-- Left Side Of Navbar -->--}}
+                {{--<ul class="nav navbar-nav">--}}
+                    {{--<li><a href="{{ url('/home') }}">Inicio</a></li>--}}
+                {{--</ul>--}}
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/facebook/login') }}">Ingresar</a></li>
-                        <li><a href="{{ url('/register') }}">Registro</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+                {{--<!-- Right Side Of Navbar -->--}}
+                {{--<ul class="nav navbar-nav navbar-right">--}}
+                    {{--<!-- Authentication Links -->--}}
+                    {{--@if (Auth::guest())--}}
+                        {{--<li><a href="{{ url('/facebook/login') }}">Ingresar</a></li>--}}
+                        {{--<li><a href="{{ url('/register') }}">Registro</a></li>--}}
+                    {{--@else--}}
+                        {{--<li class="dropdown">--}}
+                            {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
+                                {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
+                            {{--</a>--}}
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Cerrar sesión</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+                            {{--<ul class="dropdown-menu" role="menu">--}}
+                                {{--<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Cerrar sesión</a></li>--}}
+                            {{--</ul>--}}
+                        {{--</li>--}}
+                    {{--@endif--}}
+                {{--</ul>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</nav>--}}
 
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.scrollex.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.scrolly.min.js') }}"></script>
+    <script src="{{ asset('assets/js/skel.min.js') }}"></script>
+    <script src="{{ asset('assets/js/util.js') }}"></script>
+    <!--[if lte IE 8]><script src="{{ asset('assets/js/ie/respond.min.js') }}"></script><![endif]-->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
     @yield('scripts')
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,154 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-info">
-                <div class="panel-heading">Welcome</div>
+<!-- Page Wrapper -->
+<div id="page-wrapper">
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
+    <!-- Header -->
+    <header id="header" class="alt">
+        <h1><a href="#">Tombo Fans</a></h1>
+        <nav id="nav">
+            <ul>
+                <li class="special">
+                    <a href="#menu" class="menuToggle"><span>Menu</span></a>
+                    <div id="menu">
+                        <ul>
+                            <li><a href="{{ url('/') }}">Inicio</a></li>
+                            {{--<li><a href="generic.html">Generic</a></li>--}}
+                            {{--<li><a href="elements.html">Elements</a></li>--}}
+                            <li><a href="#">Registro</a></li>
+                            <li><a href="{{ url('/login') }}">Ingresar</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Banner -->
+    <section id="banner">
+        <div class="inner">
+            <h2>Tombo Fans</h2>
+            <p>La forma más fácil de impulsar tus ventas<br />
+                e interacciones en tu fanpage,<br />
+                además de conseguir <a href="#">clientes más felices</a>.</p>
+            <ul class="actions">
+                <li><a href="#" class="button special">Ingresar</a></li>
+            </ul>
         </div>
-    </div>
+        <a href="#one" class="more scrolly">Ver más</a>
+    </section>
+
+    <!-- One -->
+    <section id="one" class="wrapper style1 special">
+        <div class="inner">
+            <header class="major">
+                <h2>¿Qué es TomboFans?</h2>
+                <p>Es la forma más fácil de impulsar tus ventas e interacciones en tu fanpage, además de conseguir clientes más felices cuando ganan tus promociones y descuentos.</p>
+            </header>
+            <ul class="icons major">
+                <li><span class="icon fa-diamond major style1"><span class="label">Lorem</span></span></li>
+                <li><span class="icon fa-heart-o major style2"><span class="label">Ipsum</span></span></li>
+                <li><span class="icon fa-code major style3"><span class="label">Dolor</span></span></li>
+            </ul>
+        </div>
+    </section>
+
+    <!-- Two -->
+    <section id="two" class="wrapper alt style2">
+        <section class="spotlight">
+            <div class="image"><img src="{{ asset('images/pic01.jpg') }}" alt="" /></div><div class="content">
+                <h3>Si eres emprendedora o emprendedor:</h3>
+                <ul>
+                    <li>Incrementar tus prospectos y  tus ventas cuando invitas a participar en TomboFans.</li>
+                    <li>Aumentas las interacciones de tus clientes en tu página de fans en facebook.</li>
+                    <li>Consigues testimonios de tus clientes en facebook que te generarán más ventas.</li>
+                    <li>Fidelizas a tus clientes, te compran más veces y los tienes cautivos en tu facebook.</li>
+                </ul>
+            </div>
+        </section>
+        <section class="spotlight">
+            <div class="image"><img src="{{ asset('images/pic02.jpg') }}" alt="" /></div><div class="content">
+                <h3>Si eres participante de TomboFans:</h3>
+                <ul>
+                    <li>Obtienes descuentos o promociones GRATIS cuando te invitan a participar.</li>
+                    <li>Te enteras en facebook de las novedades de los productos o servicios que compras.</li>
+                    <li>Tienes los datos de los emprendedores que venden productos o servicios.</li>
+                </ul>
+            </div>
+        </section>
+        {{--<section class="spotlight">--}}
+            {{--<div class="image"><img src="{{ asset('images/pic03.jpg') }}" alt="" /></div><div class="content">--}}
+                {{--<h2>Augue eleifend aliquet<br />--}}
+                    {{--sed condimentum</h2>--}}
+                {{--<p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>--}}
+            {{--</div>--}}
+        {{--</section>--}}
+    </section>
+
+    <!-- Three -->
+    <section id="three" class="wrapper style3 special">
+        <div class="inner">
+            <header class="major">
+                <h2>¿Cómo funciona?</h2>
+                <p>En menos de 3 minutos configuras tu TomboFan, siguiendo estos simples pasos:</p>
+            </header>
+            <ul class="features">
+                <li class="icon fa-laptop">
+                    <h3>Paso 1</h3>
+                    <p>Escribe tu promoción y sube una imagen.</p>
+                </li>
+                <li class="icon fa-paper-plane-o">
+                    <h3>Paso 2</h3>
+                    <p>Invita a que participen en tu TomboFan (fans, amigos, clientes anteriores y prospectos).</p>
+                </li>
+                <li class="icon fa-code">
+                    <h3>Paso 3</h3>
+                    <p>Tus participantes, si tienen suerte, ganan tus promociones y/o descuentos.</p>
+                </li>
+                <li class="icon fa-flag-o">
+                    <h3>Paso 4</h3>
+                    <p>Ve como aumentan tus ventas y ademas los likes e interacciones en tu fanpage.</p>
+                </li>
+                <li class="icon fa-mail-forward">
+                    <h3>Paso 5</h3>
+                    <p>Recibes por correo los datos de tus participantes para que los contactes.</p>
+                </li>
+                <li class="icon fa-heart-o">
+                    <h3>Finalmente</h3>
+                    <p>Obtienes múltiples beneficios y tus clientes están más satisfechos.</p>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <!-- CTA -->
+    <section id="cta" class="wrapper style4">
+        <div class="inner">
+            <header>
+                <h2>¿Aún no te has convencido?</h2>
+                <p>Mira nuestra sección de casos de éxito, de otros emprendedores que ya han usado TomboFans.</p>
+            </header>
+            <ul class="actions vertical">
+                <li><a href="#" class="button fit special">Registrarme</a></li>
+                <li><a href="#" class="button fit">Ver casos</a></li>
+            </ul>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <ul class="icons">
+            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+            <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+        </ul>
+        <ul class="copyright">
+            <li>&copy; TomboFans 2017</li><li>Diseño basado en Spectral</li>
+        </ul>
+    </footer>
+
 </div>
 @endsection
