4f74aa3432a06286e79d125f20066b146ecaa128
Juan Carlos <juancagb.17@gmail.com>
2017-01-07 16:49:26 -0500

    Test options label

--- public/assets/config/step-by-step.js
+++ public/assets/config/step-by-step.js
@@ -1,3 +1,11 @@
 $(function () {
-    introJs().start();
+    var intro = introJs();
+    intro.setOption('doneLabel', 'Continuar configuración')
+        .setOption('nextLabel', 'Siguiente')
+        .setOption('prevLabel', 'Anterior')
+        .setOption('skipLabel', 'Cerrar');
+
+    intro.start().oncomplete(function() {
+        window.location.href = '/config';
+    });
 });
