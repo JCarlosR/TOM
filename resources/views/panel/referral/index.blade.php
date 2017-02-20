b227b43f1cc1074f7d33c51f073e90b752362ff5
Juan Carlos <juancagb.17@gmail.com>
2017-02-20 15:02:19 -0500

    Referrals info

--- resources/views/panel/referral/index.blade.php
+++ resources/views/panel/referral/index.blade.php
@@ -14,15 +14,54 @@
                 @endif
 
                 <p>Bienvenido a tu sección de referidos, {{ auth()->user()->name }}.</p>
-                <p>Desde esta página podrás ver un listado de los usuarios que se han registrado gracias a tu invitación.</p>
 
-                <p>Obtener referidos es tan sencillo como compartir el siguiente enlace con tus amigos:</p>
+                <p>
+                    Aquí podrás ganar dinero de manera mensual y constante (sí, como una renta),
+                    simplemente por recomendar o referir tombofans con tus amigos, clientes o cualquier persona
+                    que conozcas que tenga una página de fans, y quiera aumentar sus likes (me gusta),
+                    checkins (estoy aquí), y shares (que otros compartan publicaciones en sus muros de facebook);
+                    además de generar leads, aumentar prospectos y tener más clientes usando su página de fans.
+                </p>
+
+                <blockquote>
+                    Tu ganas el 15% de las suscripciones mensuales de aquellos que se registraron
+                    por medio de tu link (ubicado en la parte inferior), y los ganarás de manera recurrente,
+                    siempre y cuando los usuarios que referiste o afiliaste estén pagando mes a mes su suscripción.
+                </blockquote>
+
+                <p class="small">Todas tus ganancias las recibirás en tu cuenta de paypal.</p>
+
+                <p>
+                    Obtener referidos es muy fácil, porque tombofans por ahora no tiene competencia
+                    y tú puedes explicarles los beneficios de tener una aplicación muy sencilla
+                    desde su página de fans o fanpage a un costo bajo, comparado con otros mecanismos
+                    de publicidad. Solamente debes compartir el siguiente enlace con todas las personas que tengan una página de fans:
+                </p>
                 <input type="text" class="form-control" value="{{ url(auth()->user()->referral_link) }}">
 
-                <p>Si lo deseas también puedes incluir el siguiente código HTML en tu sitio web para incluir un banner y aumentar tus estadísticas:</p>
+                <p>Si lo deseas también puedes incluir el siguiente código HTML en tu sitio web para incluir un banner y aumentar tu número de afiliados:</p>
                 <textarea class="form-control"><a href="{{ url(auth()->user()->referral_link) }}">Regístrate en TomboFans, aumenta tus ventas y consigue clientes más felices!</a></textarea>
 
+                <hr>
 
+                    <p>En las siguientes ligas encontrarás contenido que te puede ayudar para recomendar a TomboFans:</p>
+                    <ul>
+                        <li>
+                            <a href="https://www.youtube.com/watch?v=TLfDkhq8HVQ">
+                                Video promocional
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/watch?v=daMaXaGANBU">
+                                Video de funcionamiento
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://tombofans.com/stories">
+                                Casos de éxito
+                            </a>
+                        </li>
+                    </ul>
             </div>
         </div>
 
@@ -38,7 +77,7 @@
                     </a>
                 </div>
 
-                <p>Información sobre los usuarios referidos.</p>
+                <p>Desde esta página podrás ver un listado de los usuarios que se han registrado gracias a tu invitación.</p>
 
                 <div class="table-responsive">
                     <table class="table table-hover table-bordered">
