e1094e0acc98b035e8efbfcf3ffb5daaedb7ec60
Juan Carlos <juancagb.17@gmail.com>
2017-02-28 07:53:43 -0500

    Fb fix

--- app/Http/Controllers/Guess/PromotionController.php
+++ app/Http/Controllers/Guess/PromotionController.php
@@ -9,6 +9,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
+use SammyK\LaravelFacebookSdk\LaravelFacebookSdk;
 
 class PromotionController extends Controller
 {
