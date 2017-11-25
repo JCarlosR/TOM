b91353cbdccc2999111a99ad400e936dcf30a384
Juan Carlos <juancagb.17@gmail.com>
2017-11-25 16:06:43 -0500

    Send output to

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('posts:send')->everyMinute();
+        $schedule->command('posts:send')
+            ->everyMinute()->sendOutputTo('scheduled_posts.log');;
     }
 }
